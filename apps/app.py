65bffe3b59dcf3241a673f68d25625691934288d
Muhsin Shah <111670341+Muhsin95952@users.noreply.github.com>
2025-07-14 12:50:45 +0500

    Update app.py

--- apps/app.py
+++ apps/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from Flask CI/CD pipeline!. I am Muhsin shah and you are waching my CICD pipeline."
+    return "Hello from Flask CI/CD pipeline!. I am Muhsin shah and you are waching my CICD pipeline. and how are you ??"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
